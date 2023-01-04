554e51f190114084ce5e37f2ae9cceba2e78bd48
Jonas Brito <jonasexplore@gmail.com>
2023-01-04 17:44:59 -0300

    Delete routes.js

--- public/routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { custodyController } = require("./controller");
-
-const router = Router();
-
-router.post("/custody", custodyController);
-
-module.exports = { router };
