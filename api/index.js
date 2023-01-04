3aff3e6135281e51003296e18a243de9354731c9
Jonas Brito <jonasexplore@gmail.com>
2023-01-04 17:56:35 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -7,6 +7,6 @@ const custodyController = (req, res) => {
   console.log(req.body);
   return res.sendStatus(200);
 };
-app.post("/custody", custodyController);
+app.post("/api/custody", custodyController);
 
 app.listen(3000, () => console.log("🔮 application has started."));
