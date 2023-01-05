0f91c45e287d9edcf6dd66c351d4eafb1325cb3a
Jonas Brito <jonasexplore@gmail.com>
2023-01-05 10:02:21 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,33 +1,19 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 
 const app = express();
 app.use(express.json());
 
 const custodyController = (req, res) => {
   try {
-    if (!fs.existsSync(path.join(__dirname, "../tmp"))) {
-      fs.mkdirSync(path.join(__dirname, "../tmp"));
-    }
-
-    const filePath = path.join(__dirname, "../tmp", "custody.txt");
     if (req.body?.challenge) {
-      fs.writeFile(filePath, JSON.stringify(req.body), function (err) {
-        if (err) {
-          console.log(err);
-          throw err;
-        }
-        console.log("Saved!");
-      });
-
+      console.log(req.body);
       return res.send(req.body.challenge);
     }
 
     return res.sendStatus(200);
   } catch (error) {
     console.log(error);
-    return res.send(error).status(500);
+    return res.json({ message: error?.message }).status(500);
   }
 };
 app.post("/api/custody", custodyController);
