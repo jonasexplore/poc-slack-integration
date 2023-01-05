2fd407de1591c59c5040a03edeb4df569ccfdc90
Jonas Brito <jonasexplore@gmail.com>
2023-01-05 12:33:06 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,16 +1,40 @@
+const { request } = require("express");
 const express = require("express");
+const axios = require("axios").default;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const custodyController = (req, res) => {
+const custodyController = async (req, res) => {
   try {
     const resquest = req.body;
-    console.log("INFO >> BODY >> ", resquest);
+    // console.log("INFO >> BODY >> ", resquest);
 
     if (resquest?.challenge) {
       return res.send(resquest.challenge);
+    }
+
+    if (request?.payload) {
+      const payload = request.payload;
+
+      if (payload.type === "message_action") {
+        console.log(
+          "User: ",
+          payload?.user?.name,
+          "Request",
+          payload?.message?.text,
+          "Callback ID: ",
+          payload?.callback_id
+        );
+
+        const messageResponse =
+          "Recebemos sua solicitação! Iremos marcar que a transferência foi realizada com sucesso, muito obrigado.";
+
+        await axios.post(payload.response_url, {
+          text: messageResponse,
+        });
+      }
     }
 
     return res.status(200).send();
