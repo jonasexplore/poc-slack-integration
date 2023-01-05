0691cd7e650f1a0a8de5b270f71512c73bf6e423
Jonas Brito <jonasexplore@gmail.com>
2023-01-05 12:22:22 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const custodyController = (req, res) => {
   try {
-    const resquest = JSON.parse(req.body);
+    const resquest = req.body;
     console.log("INFO >> BODY >> ", resquest);
 
     if (resquest?.challenge) {
