a58a31c2ac9aa1e148e59bad08b174aadaeaaaf0
Jonas Brito <jonasexplore@gmail.com>
2023-01-05 10:59:06 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -5,17 +5,21 @@ app.use(express.json());
 
 const custodyController = (req, res) => {
   try {
-    console.log(req.body);
+    console.log("INFO >> BODY >> ", req.body);
+
     if (req.body?.challenge) {
       return res.send(req.body.challenge);
     }
 
-    return res.sendStatus(200);
+    return res.json({
+      text: "Recebemos a solicição de marcação de conclusão da transferência de custódia, Obrigado.",
+    });
   } catch (error) {
-    console.log(error);
+    console.log("ERROR >> ", error);
     return res.json({ message: error?.message }).status(500);
   }
 };
+
 app.post("/api/custody", custodyController);
 
 app.listen(3000, () => console.log("🔮 application has started."));
